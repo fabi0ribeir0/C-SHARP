c83d01574112df615c4f3ace0887fd22570c36b8
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-04-27 17:30:32 -0300

    Verificar se int é numero

--- MeuSistema/Form1.Designer.cs
+++ MeuSistema/Form1.Designer.cs
@@ -28,49 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnOK = new System.Windows.Forms.Button();
-            this.btnCancelar = new System.Windows.Forms.Button();
-            this.txtFrase = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtParcela1 = new System.Windows.Forms.TextBox();
+            this.txtParcela2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnOK
+            // label1
             // 
-            this.btnOK.Location = new System.Drawing.Point(90, 146);
-            this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
-            this.btnOK.TabIndex = 0;
-            this.btnOK.Text = "OK";
-            this.btnOK.UseVisualStyleBackColor = true;
-            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Parcela 1";
             // 
-            // btnCancelar
+            // txtParcela1
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(259, 146);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
-            this.btnCancelar.TabIndex = 1;
-            this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
-            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            this.txtParcela1.Location = new System.Drawing.Point(88, 25);
+            this.txtParcela1.Name = "txtParcela1";
+            this.txtParcela1.Size = new System.Drawing.Size(100, 20);
+            this.txtParcela1.TabIndex = 1;
             // 
-            // txtFrase
+            // txtParcela2
             // 
-            this.txtFrase.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtFrase.Location = new System.Drawing.Point(49, 20);
-            this.txtFrase.Name = "txtFrase";
-            this.txtFrase.Size = new System.Drawing.Size(294, 20);
-            this.txtFrase.TabIndex = 2;
-            this.txtFrase.Tag = "";
-            this.txtFrase.Text = "Digite seu nome";
+            this.txtParcela2.Location = new System.Drawing.Point(88, 51);
+            this.txtParcela2.Name = "txtParcela2";
+            this.txtParcela2.Size = new System.Drawing.Size(100, 20);
+            this.txtParcela2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Parcela 2";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(88, 77);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(194, 75);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 6;
+            this.btnCalc.Text = "Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtFrase);
-            this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.btnOK);
+            this.ClientSize = new System.Drawing.Size(319, 155);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtParcela2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtParcela1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Aplicação";
             this.ResumeLayout(false);
@@ -80,9 +116,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnOK;
-        private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.TextBox txtFrase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtParcela1;
+        private System.Windows.Forms.TextBox txtParcela2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalc;
     }
 }
 
--- MeuSistema/Form1.cs
+++ MeuSistema/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,19 +13,34 @@ namespace MeuSistema
 {
     public partial class Form1 : Form
     {
+
+        // Converção e operadores aritiméticos
+
+        int valor1, valor2, resultado;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btnOK_Click(object sender, EventArgs e)
+        private void btnCalc_Click(object sender, EventArgs e)
         {
-            txtFrase.Text = "Fábio";
-        }
+            bool verifica;
+                      
+            verifica = int.TryParse(txtParcela1.Text, out valor1);
+            if (verifica == false)
+            {
+                MessageBox.Show("Caracter invalido no campo Valor 1, digite um numero");
+            }
+            verifica = int.TryParse(txtParcela2.Text, out valor2);
+            if (verifica == false)
+            {
+                MessageBox.Show("Caracter invalido no campo Valor 2, digite um numero");
+            }
 
-        private void btnCancelar_Click(object sender, EventArgs e)
-        {
-            txtFrase.Text = "";
+
+            resultado = valor1 + valor2;
+            txtTotal.Text = resultado.ToString();
         }
     }
 }
