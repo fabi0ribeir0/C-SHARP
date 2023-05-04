03684a6ffda2e16dacce5d1f9f86781c1b1316a5
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-04 14:55:00 -0300

    metodo p2

---
+++ Metodop2/NomeCompleto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metodop2
+{
+    internal class NomeCompleto
+    {
+        public string nome, sobreNome;
+
+        public void NomeInteiro()
+        {
+            MessageBox.Show(Resposta());
+        }
+
+        private string Resposta()
+        {
+            string completo = nome + " " + sobreNome;
+            return completo;
+        }
+    }
+}
