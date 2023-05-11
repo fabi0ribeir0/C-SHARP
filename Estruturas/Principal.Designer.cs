92e32fc6fa05905586902f32de04f280c7938b58
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-11 11:33:01 -0300

    Estruturas

---
+++ Estruturas/Principal.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Estruturas
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnB1 = new System.Windows.Forms.Button();
+            this.btnB2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnB1
+            // 
+            this.btnB1.Location = new System.Drawing.Point(83, 441);
+            this.btnB1.Name = "btnB1";
+            this.btnB1.Size = new System.Drawing.Size(75, 23);
+            this.btnB1.TabIndex = 0;
+            this.btnB1.Text = "B1";
+            this.btnB1.UseVisualStyleBackColor = true;
+            this.btnB1.Click += new System.EventHandler(this.btnB1_Click);
+            // 
+            // btnB2
+            // 
+            this.btnB2.Location = new System.Drawing.Point(247, 441);
+            this.btnB2.Name = "btnB2";
+            this.btnB2.Size = new System.Drawing.Size(75, 23);
+            this.btnB2.TabIndex = 1;
+            this.btnB2.Text = "B2";
+            this.btnB2.UseVisualStyleBackColor = true;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 518);
+            this.Controls.Add(this.btnB2);
+            this.Controls.Add(this.btnB1);
+            this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Titulo X";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnB1;
+        private System.Windows.Forms.Button btnB2;
+    }
+}
+
---
+++ Estruturas/Principal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estruturas
+{
+    public partial class Principal : Form
+    {//Global
+        struct EstruturaPessoal
+        {
+            public string Nome;
+            public string Sobre;
+        }
+
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnB1_Click(object sender, EventArgs e)
+        {
+            EstruturaPessoal ep = new EstruturaPessoal();
+            ep.Nome = "Fábio";
+            ep.Sobre = "Ribeiro";
+
+            MessageBox.Show($"Eu {ep.Nome} {ep.Sobre}, estou estudando estruturas");
+        }
+    }
+}
