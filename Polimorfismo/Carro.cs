49fa7109bde1d04847a745aa1fc8cac5c1f2c674
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-15 16:23:50 -0300

    Abstract

---
+++ Polimorfismo/Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polimorfismo
+{
+    internal class Carro : Veiculos
+    {
+        public string radiador { get; set; }
+
+        public override void acelerar()
+        {
+            MessageBox.Show("Carro acelerou");
+        }
+    }
+}
---
+++ Polimorfismo/Veiculos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polimorfismo
+{
+    abstract class Veiculos
+    {
+        public string cor { get; set; }
+        public string modelo { get; set; }
+
+        abstract public void acelerar();
+    }
+}
