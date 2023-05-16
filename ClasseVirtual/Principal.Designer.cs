321423556e15df5404ea3c679e762f926c599826
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-16 10:52:13 -0300

    Criado Btns e Classe Master

--- ClasseVirtual/Principal.Designer.cs
+++ ClasseVirtual/Principal.Designer.cs
@@ -28,14 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnTeste1 = new System.Windows.Forms.Button();
+            this.btnTeste2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnTeste1
+            // 
+            this.btnTeste1.Location = new System.Drawing.Point(61, 370);
+            this.btnTeste1.Name = "btnTeste1";
+            this.btnTeste1.Size = new System.Drawing.Size(75, 23);
+            this.btnTeste1.TabIndex = 0;
+            this.btnTeste1.Text = "Teste1";
+            this.btnTeste1.UseVisualStyleBackColor = true;
+            // 
+            // btnTeste2
+            // 
+            this.btnTeste2.Location = new System.Drawing.Point(230, 370);
+            this.btnTeste2.Name = "btnTeste2";
+            this.btnTeste2.Size = new System.Drawing.Size(75, 23);
+            this.btnTeste2.TabIndex = 1;
+            this.btnTeste2.Text = "Teste2";
+            this.btnTeste2.UseVisualStyleBackColor = true;
+            // 
+            // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 543);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(411, 479);
+            this.Controls.Add(this.btnTeste2);
+            this.Controls.Add(this.btnTeste1);
+            this.Name = "Principal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Titulo";
             this.ResumeLayout(false);
@@ -43,6 +65,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTeste1;
+        private System.Windows.Forms.Button btnTeste2;
     }
 }
 
