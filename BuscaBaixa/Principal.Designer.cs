f0fb6c2aaedb3da874d67ea02f256349516e65a0
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-19 08:06:16 -0300

    inicio projeto

---
+++ BuscaBaixa/Principal.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace BuscaBaixa
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txttermoBusca = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(152, 325);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txttermoBusca
+            // 
+            this.txttermoBusca.Location = new System.Drawing.Point(54, 182);
+            this.txttermoBusca.Name = "txttermoBusca";
+            this.txttermoBusca.Size = new System.Drawing.Size(557, 20);
+            this.txttermoBusca.TabIndex = 1;
+            this.txttermoBusca.Text = "o melhor p";
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 558);
+            this.Controls.Add(this.txttermoBusca);
+            this.Controls.Add(this.btnBuscar);
+            this.Name = "Principal";
+            this.Text = "Buscador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txttermoBusca;
+    }
+}
+
---
+++ BuscaBaixa/Principal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuscaBaixa
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string url = "https://medicalway.com.br/";
+            string termoBusca = txttermoBusca.Text;
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    string conteudoPagina = client.DownloadString(url);
+
+                    if (conteudoPagina.Contains(termoBusca))
+                    {
+                        MessageBox.Show("Termo Encontrado!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Termo não encontrado!");
+                    }
+                }
+                catch (WebException ex)
+                {
+
+                    MessageBox.Show("Ocorreu um erro ao acessar o site: ");
+                }
+            }
+
+
+        }
+    }
+}
