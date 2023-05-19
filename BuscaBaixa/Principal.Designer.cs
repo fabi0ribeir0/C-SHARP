426d4e2fa7454477ea9592951c5828446a54e45b
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-19 11:31:28 -0300

    ainda não funcionaou

--- BuscaBaixa/Principal.Designer.cs
+++ BuscaBaixa/Principal.Designer.cs
@@ -29,12 +29,17 @@
         private void InitializeComponent()
         {
             this.btnBuscar = new System.Windows.Forms.Button();
-            this.txttermoBusca = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSite = new System.Windows.Forms.TextBox();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.btnProximo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(152, 325);
+            this.btnBuscar.Location = new System.Drawing.Point(693, 65);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(75, 23);
             this.btnBuscar.TabIndex = 0;
@@ -42,23 +47,66 @@
             this.btnBuscar.UseVisualStyleBackColor = true;
             this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
-            // txttermoBusca
+            // pictureBox1
             // 
-            this.txttermoBusca.Location = new System.Drawing.Point(54, 182);
-            this.txttermoBusca.Name = "txttermoBusca";
-            this.txttermoBusca.Size = new System.Drawing.Size(557, 20);
-            this.txttermoBusca.TabIndex = 1;
-            this.txttermoBusca.Text = "o melhor p";
+            this.pictureBox1.Location = new System.Drawing.Point(90, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(642, 377);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Escolha o Site";
+            // 
+            // txtSite
+            // 
+            this.txtSite.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtSite.Location = new System.Drawing.Point(132, 67);
+            this.txtSite.Name = "txtSite";
+            this.txtSite.Size = new System.Drawing.Size(555, 20);
+            this.txtSite.TabIndex = 3;
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Location = new System.Drawing.Point(54, 301);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(30, 24);
+            this.btnAnterior.TabIndex = 4;
+            this.btnAnterior.Text = "<";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            this.btnAnterior.Click += new System.EventHandler(this.btnAnterior_Click);
+            // 
+            // btnProximo
+            // 
+            this.btnProximo.Location = new System.Drawing.Point(738, 301);
+            this.btnProximo.Name = "btnProximo";
+            this.btnProximo.Size = new System.Drawing.Size(30, 24);
+            this.btnProximo.TabIndex = 5;
+            this.btnProximo.Text = ">";
+            this.btnProximo.UseVisualStyleBackColor = true;
+            this.btnProximo.Click += new System.EventHandler(this.btnProximo_Click);
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(858, 558);
-            this.Controls.Add(this.txttermoBusca);
+            this.Controls.Add(this.btnProximo);
+            this.Controls.Add(this.btnAnterior);
+            this.Controls.Add(this.txtSite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnBuscar);
             this.Name = "Principal";
             this.Text = "Buscador";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,7 +115,11 @@
         #endregion
 
         private System.Windows.Forms.Button btnBuscar;
-        private System.Windows.Forms.TextBox txttermoBusca;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSite;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.Button btnProximo;
     }
 }
 
--- BuscaBaixa/Principal.cs
+++ BuscaBaixa/Principal.cs
@@ -1,51 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
 
 namespace BuscaBaixa
 {
     public partial class Principal : Form
     {
+        private List<string> urlsImagens;
+        private int indiceAtual;
+
         public Principal()
         {
             InitializeComponent();
         }
 
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            // Carregar as URLs de imagem do arquivo texto para a lista urlsImagens
+            urlsImagens = new List<string>();
+            indiceAtual = 0;
+            ExibirImagemAtual();
+        }
+
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            string url = "https://medicalway.com.br/";
-            string termoBusca = txttermoBusca.Text;
+            string url = $"https://{txtSite.Text}";
+            string caminhoDestino = $"C:\\Users\\Fabio\\Documents\\img\\{txtSite.Text}.txt";
 
-            using (WebClient client = new WebClient())
+            try
             {
-                try
-                {
-                    string conteudoPagina = client.DownloadString(url);
+                HtmlWeb web = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument document = web.Load(url);
 
-                    if (conteudoPagina.Contains(termoBusca))
-                    {
-                        MessageBox.Show("Termo Encontrado!");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Termo não encontrado!");
-                    }
-                }
-                catch (WebException ex)
-                {
+                // Extrair todas as tags <img> do HTML
+                var imagens = document.DocumentNode.Descendants("img");
 
-                    MessageBox.Show("Ocorreu um erro ao acessar o site: ");
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var img in imagens)
+                {
+                    string urlImagem = img.GetAttributeValue("src", "");
+                    sb.AppendLine(urlImagem);
                 }
+
+                File.WriteAllText(caminhoDestino, sb.ToString());
+                MessageBox.Show("Exportado");
+
+                // Carregar as URLs de imagem do arquivo de texto para a lista urlsImagens
+                urlsImagens = File.ReadAllLines(caminhoDestino).ToList();
+                indiceAtual = 0;
+                ExibirImagemAtual();
+
             }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao acessar o site: " + ex.Message);
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao salvar o arquivo de texto: " + ex.Message);
+            }
+        }
 
+        private void ExibirImagemAtual()
+        {
+            if (indiceAtual >= 0 && indiceAtual < urlsImagens.Count)
+            {
+                // Carregar a imagem a partir da URL e atribuí-la ao PictureBox
+                string urlImagemAtual = urlsImagens[indiceAtual];
+                pictureBox1.Load(urlImagemAtual);
+            }
+        }
 
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            if (indiceAtual > 0)
+            {
+                indiceAtual--;
+                ExibirImagemAtual();
+            }
+        }
+
+        private void btnProximo_Click(object sender, EventArgs e)
+        {
+            if (indiceAtual < urlsImagens.Count - 1)
+            {
+                indiceAtual++;
+                ExibirImagemAtual();
+            }
         }
     }
 }
