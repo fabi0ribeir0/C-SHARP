47b88c030bbc107919fd8bf8537553f9ca4c26c5
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-22 17:33:02 -0300

    Mudar localização via codigo

--- FrmMdiPai/Pai.Designer.cs
+++ FrmMdiPai/Pai.Designer.cs
@@ -31,6 +31,8 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mDIFilhoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnMuda = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,11 +61,32 @@
             this.mDIFilhoToolStripMenuItem.Text = "MDI Filho";
             this.mDIFilhoToolStripMenuItem.Click += new System.EventHandler(this.mDIFilhoToolStripMenuItem_Click);
             // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(60, 87);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnMuda
+            // 
+            this.btnMuda.Location = new System.Drawing.Point(12, 196);
+            this.btnMuda.Name = "btnMuda";
+            this.btnMuda.Size = new System.Drawing.Size(200, 200);
+            this.btnMuda.TabIndex = 3;
+            this.btnMuda.Text = "Muda";
+            this.btnMuda.UseVisualStyleBackColor = true;
+            // 
             // Pai
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(833, 531);
+            this.Controls.Add(this.btnMuda);
+            this.Controls.Add(this.btnOK);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
@@ -81,6 +104,8 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem mDIFilhoToolStripMenuItem;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnMuda;
     }
 }
 
--- FrmMdiPai/Pai.cs
+++ FrmMdiPai/Pai.cs
@@ -25,5 +25,18 @@ namespace FrmMdiPai
             FrmFilho frm = new FrmFilho(this);
             frm.Show();
         }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            int larg = this.DisplayRectangle.Width;
+            int alt = this.DisplayRectangle.Height;
+
+            //MessageBox.Show(larg + "de largura por " + alt + " de altura.");
+
+            //int bAltura = btnMuda.Size.Height/2;
+            //int bLargura = btnMuda.Size.Width/2;
+
+            btnMuda.Location = new Point(larg/2-btnMuda.Width/2, alt/2-btnMuda.Height/2);
+        }
     }
 }
