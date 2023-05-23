4bade6c85a61896d73087dae67122ef81bff8d09
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-23 14:33:13 -0300

    DtmEstruturas

---
+++ DtmEstruturas/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace DtmEstruturas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblData = new System.Windows.Forms.Label();
+            this.lblAtual = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(299, 361);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(296, 422);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(65, 13);
+            this.lblData.TabIndex = 1;
+            this.lblData.Text = "00/00/0000";
+            // 
+            // lblAtual
+            // 
+            this.lblAtual.AutoSize = true;
+            this.lblAtual.Location = new System.Drawing.Point(653, 58);
+            this.lblAtual.Name = "lblAtual";
+            this.lblAtual.Size = new System.Drawing.Size(65, 13);
+            this.lblAtual.TabIndex = 2;
+            this.lblAtual.Text = "00/00/0000";
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(12, 270);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(100, 20);
+            this.txtDia.TabIndex = 3;
+            this.txtDia.Text = "01";
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(12, 333);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(100, 20);
+            this.txtMes.TabIndex = 4;
+            this.txtMes.Text = "01";
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(12, 392);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(100, 20);
+            this.txtAno.TabIndex = 5;
+            this.txtAno.Text = "1800";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.lblAtual);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form1";
+            this.Text = "Principal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblAtual;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DtmEstruturas/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DtmEstruturas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblAtual.Text = DateTime.Now.ToString();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            int d = 0;
+            int m = 0;
+            int a = 0;
+
+            d = int.Parse(txtDia.Text);
+            m = int.Parse(txtMes.Text);
+            a = int.Parse(txtAno.Text);
+
+
+            //lblData.Text = DateTime.Today.ToString("yy");
+
+            DateTime dt = new DateTime(a, m, d);
+
+            lblData.Text = dt.DayOfYear.ToString();
+            MessageBox.Show(dt.DayOfWeek.ToString());
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblAtual.Text = DateTime.Now.ToString("dd/mm/yyyy \nhh:mm:ss");
+        }
+    }
+}
