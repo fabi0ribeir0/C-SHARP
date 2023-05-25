6d160585eba747f6c737cf8138b2a8e99a7191b4
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-25 16:13:37 -0300

    criardiretorio

--- CriandoDiretorioseAqriovos/Principal.cs
+++ CriandoDiretorioseAqriovos/Principal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,23 @@ namespace CriandoDiretorioseAqriovos
 {
     public partial class Principal : Form
     {
+        string caminho = @"C:\ARQ\BACKUP";
         public Principal()
         {
             InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            //Criar pasta
+            
+            if (Directory.Exists(caminho))
+            {
+                MessageBox.Show("EXISTE");
+            }
+            else { Directory.CreateDirectory(caminho); MessageBox.Show("Criado"); }
+
+            //Directory
         }
     }
 }
