40bfdc20828a95f492096d738ea68006fa06a3ae
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-26 10:22:37 -0300

    DialogResult

---
+++ DialogResult/Principal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogResult
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+                                     //Texto            Título
+            var res = MessageBox.Show("Desaja sair ?", "ENCERRAR", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            // por algum motivo tive que declarar o System.Windows.Form para funcionar
+            if (res == System.Windows.Forms.DialogResult.Yes) 
+                                                              
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
