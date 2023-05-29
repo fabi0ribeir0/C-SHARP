0186d356d6e38a7315a1ed37ddaeed6cbd0e6657
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-29 17:06:27 -0300

    Finish to day

---
+++ ListaColecoes/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaColecoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+                    /*LIST*/
+
+            List<string> Nomes = new List<string>();
+
+            Nomes.Add("Teste 1");
+            Nomes.Add("Teste 2");
+            Nomes.Add("Teste 3");
+
+
+            foreach (string Nome in Nomes)
+            {
+                lstLista.Items.Add(Nome);
+            }
+
+        }
+    }
+}
