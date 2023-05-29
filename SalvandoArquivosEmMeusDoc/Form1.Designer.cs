3bc7ee43c6a3427c9297b926c4acdb5b1c90c390
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-29 11:14:23 -0300

    MeioInutiu

--- SalvandoArquivosEmMeusDoc/Form1.Designer.cs
+++ SalvandoArquivosEmMeusDoc/Form1.Designer.cs
@@ -29,32 +29,56 @@
         private void InitializeComponent()
         {
             this.btnOK = new System.Windows.Forms.Button();
+            this.txtNomeArquivo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(128, 357);
+            this.btnOK.Location = new System.Drawing.Point(143, 355);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(75, 23);
             this.btnOK.TabIndex = 0;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtNomeArquivo
+            // 
+            this.txtNomeArquivo.Location = new System.Drawing.Point(46, 300);
+            this.txtNomeArquivo.Name = "txtNomeArquivo";
+            this.txtNomeArquivo.Size = new System.Drawing.Size(278, 20);
+            this.txtNomeArquivo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome do Arquivo";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(381, 438);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNomeArquivo);
             this.Controls.Add(this.btnOK);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox txtNomeArquivo;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SalvandoArquivosEmMeusDoc/Form1.cs
+++ SalvandoArquivosEmMeusDoc/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SalvandoArquivosEmMeusDoc
 {
@@ -15,6 +16,26 @@ namespace SalvandoArquivosEmMeusDoc
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog salvar = new SaveFileDialog();
+
+            salvar.Title = "Gravar arquivo";
+            salvar.Filter = "TXT|*.txt";
+            salvar.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (salvar.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            string caminho = salvar.FileName;
+            StreamWriter stream = new StreamWriter(caminho, false, Encoding.Default);
+
+            stream.WriteLine(txtNomeArquivo.Text);
+            
+            stream.Dispose();
+
         }
     }
 }
