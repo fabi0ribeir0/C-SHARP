099b3bf8498a093397fee1d47b419089d9f91a9f
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-05-29 11:30:14 -0300

    Caixa de dialogo e cores

---
+++ FolderBroser/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace FolderBroser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCor = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(183, 358);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 321);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnCor
+            // 
+            this.btnCor.Location = new System.Drawing.Point(394, 161);
+            this.btnCor.Name = "btnCor";
+            this.btnCor.Size = new System.Drawing.Size(75, 23);
+            this.btnCor.TabIndex = 2;
+            this.btnCor.Text = "Cor";
+            this.btnCor.UseVisualStyleBackColor = true;
+            this.btnCor.Click += new System.EventHandler(this.btnCor_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(487, 134);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 449);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCor);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCor;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FolderBroser/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderBroser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog pastas = new FolderBrowserDialog();
+
+            pastas.SelectedPath = @"C:\ARQ\";
+
+            if (pastas.ShowDialog() == DialogResult.Cancel) return;
+
+            textBox1.Text = pastas.SelectedPath;
+
+        }
+
+        private void btnCor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+
+            cd.Color = pictureBox1.BackColor;
+
+            if (cd.ShowDialog() == DialogResult.Cancel) return;
+
+            pictureBox1.BackColor = cd.Color;
+
+        }
+    }
+}
