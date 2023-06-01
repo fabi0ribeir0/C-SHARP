98d0e7ba4b7ccde27f4075e70cdddf11e866746f
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-06-01 14:39:23 -0300

    imprime e fecha após

---
+++ PrintNewPage/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace PrintNewPage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.Imprimir = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(175, 456);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.TabIndex = 0;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // Imprimir
+            // 
+            this.Imprimir.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.Imprimir_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 550);
+            this.Controls.Add(this.btnImprimir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Drawing.Printing.PrintDocument Imprimir;
+    }
+}
+
---
+++ PrintNewPage/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintNewPage
+{
+    public partial class Form1 : Form
+    {
+        //Config Pagina
+        Font letra;
+        SolidBrush cor;
+        int y = 50;
+        int numFrases = 1;
+        bool isPrinting = false;
+        int altura;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Imprimir_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            // Adicionando paginas
+
+            altura = Imprimir.DefaultPageSettings.Bounds.Height;
+
+            while (numFrases <= 100)
+            {
+                e.Graphics.DrawString("Minha frase de número " + numFrases.ToString(), letra, cor, new Point(50,y));
+                y += 30;
+                numFrases++;
+
+                if (y >= altura - 70)
+                {
+                    y = 50;
+                    e.HasMorePages = true;
+                    break;
+                }
+
+            }
+
+            // Verifica se todas as páginas foram impressas
+            if (numFrases > 100)
+            {
+                isPrinting = false;
+            }
+            else
+            {
+                //Continua a imnpressão na próxima página
+                e.HasMorePages = true;
+            }
+
+            //Verifica se todas as páginas foram impressas
+            if (!isPrinting)
+            {
+                //Fecha o programa após a impressão
+                Close();
+            }
+
+        }
+
+            
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            letra = new Font("Arial", 14, FontStyle.Bold, GraphicsUnit.Pixel);
+            cor = new SolidBrush(Color.DarkBlue);
+
+            numFrases = 1;
+            isPrinting = true;
+
+            Imprimir.Print();
+        }
+    }
+}
