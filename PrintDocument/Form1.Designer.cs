6946afc04dabba389161e400d6ea7b7aa269b358
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-06-01 08:59:47 -0300

    print

--- PrintDocument/Form1.Designer.cs
+++ PrintDocument/Form1.Designer.cs
@@ -28,20 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.Impressora = new System.Drawing.Printing.PrintDocument();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(194, 369);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.TabIndex = 0;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Impressora
+            // 
+            this.Impressora.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.Impressora_PrintPage);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(341, 20);
+            this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(589, 469);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnImprimir);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Drawing.Printing.PrintDocument Impressora;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- PrintDocument/Form1.cs
+++ PrintDocument/Form1.cs
@@ -16,5 +16,21 @@ namespace PrintDocument
         {
             InitializeComponent();
         }
+
+        private void Impressora_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            string texto = textBox1.Text;
+            Font tipo_fonte = new Font("Arial", 16, FontStyle.Regular, GraphicsUnit.Pixel);
+            SolidBrush cor = new SolidBrush(Color.Black);
+            Point local = new Point(50,50);
+
+            e.Graphics.DrawString(texto, tipo_fonte, cor, local);
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Impressora.Print();
+        }
+
     }
 }
