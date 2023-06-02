62ec5a0c081e1baa7efa2f072f3ec9972f649fe2
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-06-02 13:05:51 -0300

    MegaSena

---
+++ AppMegaSena/Principal.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace AppMegaSena
+{
+    partial class FrmMegaSena
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGerar.Location = new System.Drawing.Point(114, 21);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(94, 132);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 20);
+            this.lblResultado.TabIndex = 1;
+            // 
+            // FrmMegaSena
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 246);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnGerar);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(332, 285);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(332, 285);
+            this.Name = "FrmMegaSena";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MegaSena";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ AppMegaSena/Principal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppMegaSena
+{
+    public partial class FrmMegaSena : Form
+    {
+        public FrmMegaSena()
+        {
+            InitializeComponent();
+        }
+
+        private void sorteio()
+        {
+            Random rnd = new Random();
+            int[] result = new int[6];
+            string saida = "";
+
+
+
+            for (int i = 0; i < result.Length; i++)
+            {
+
+                inicio:
+
+                int numero = rnd.Next(1, 61);
+
+                // Verifica se o número já foi gerado
+                if (result.Contains(numero))
+                    goto inicio; // se o número for repetido, gerar outro número
+
+                result[i] = numero;
+                saida += result[i] + " ";
+
+            }
+
+            lblResultado.Text = saida;
+
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            sorteio();
+        }
+    }
+}
