8c8cd37a04fa6e068ea6c3bcb85c1b8e582fa003
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-06-02 09:41:47 -0300

    random

---
+++ RandomAleatorio/Principal.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RandomAleatorio
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListar = new System.Windows.Forms.Button();
+            this.lstLista = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(397, 390);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 0;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // lstLista
+            // 
+            this.lstLista.FormattingEnabled = true;
+            this.lstLista.Location = new System.Drawing.Point(12, 12);
+            this.lstLista.Name = "lstLista";
+            this.lstLista.Size = new System.Drawing.Size(340, 511);
+            this.lstLista.TabIndex = 1;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 543);
+            this.Controls.Add(this.lstLista);
+            this.Controls.Add(this.btnListar);
+            this.Name = "Principal";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.ListBox lstLista;
+    }
+}
+
---
+++ RandomAleatorio/Principal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomAleatorio
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            //Random aleatorio = new Random();
+
+            //lstLista.Items.Clear();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    int resultado = aleatorio.Next(0,100);
+            //    lstLista.Items.Add(resultado);
+            //}
+
+            for (int i = 0; i < 10; i++)
+            {
+                lstLista.Items.Add(cod());
+            }
+
+        }
+
+        private string cod(int qtd = 30)
+        {
+            StringBuilder str = new StringBuilder();
+
+            string caracteres = "ABCDEFGHIJKLMNOPQRSTUVXZabcdefghijklmnopqrstuvxz";
+
+            for (int i = 0; i < qtd; i++)
+            {
+                Random random = new Random(i);
+                
+                int x = random.Next(caracteres.Length);
+                str.Append(caracteres[x]);
+
+            }
+
+            return str.ToString();
+            
+        }
+    }
+}
