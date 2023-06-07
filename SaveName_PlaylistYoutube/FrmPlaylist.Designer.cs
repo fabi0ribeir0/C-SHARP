605caa83aee8b5202537b061f02aa61add228cc2
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-06-07 16:36:50 -0300

    problema na autenticação

---
+++ SaveName_PlaylistYoutube/FrmPlaylist.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SaveName_PlaylistYoutube
+{
+    partial class FrmPlaylist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPlaylistUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExtrair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPlaylistUrl
+            // 
+            this.txtPlaylistUrl.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtPlaylistUrl.Location = new System.Drawing.Point(12, 56);
+            this.txtPlaylistUrl.Name = "txtPlaylistUrl";
+            this.txtPlaylistUrl.Size = new System.Drawing.Size(443, 20);
+            this.txtPlaylistUrl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL DA PLAYLIST";
+            // 
+            // btnExtrair
+            // 
+            this.btnExtrair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExtrair.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExtrair.Location = new System.Drawing.Point(12, 82);
+            this.btnExtrair.Name = "btnExtrair";
+            this.btnExtrair.Size = new System.Drawing.Size(70, 29);
+            this.btnExtrair.TabIndex = 2;
+            this.btnExtrair.Text = "Extrair";
+            this.btnExtrair.UseVisualStyleBackColor = true;
+            this.btnExtrair.Click += new System.EventHandler(this.btnExtrair_Click);
+            // 
+            // FrmPlaylist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 366);
+            this.Controls.Add(this.btnExtrair);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPlaylistUrl);
+            this.Name = "FrmPlaylist";
+            this.Text = "Baixa nomes da playlist";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPlaylistUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExtrair;
+    }
+}
+
---
+++ SaveName_PlaylistYoutube/FrmPlaylist.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace SaveName_PlaylistYoutube
+{
+    public partial class FrmPlaylist : Form
+    {
+        public FrmPlaylist()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsValidPlaylistUrl(string url)
+        {
+            return url.StartsWith("https://www.youtube.com/playlist?") || url.StartsWith("https://youtube.com/playlist?");
+        }
+
+        private string GetPlaylistId(string url)
+        {
+            string playlistId = string.Empty;
+
+            int startIndex = url.IndexOf("list=");
+            if (startIndex != -1)
+            {
+                startIndex += 5;
+                int endIndex = url.IndexOf('&', startIndex);
+                playlistId = endIndex != -1 ? url.Substring(startIndex, endIndex - startIndex) : url.Substring(startIndex);
+            }
+
+            return playlistId;
+        }
+
+        private string GetApiResponse(string playlistId)
+        {
+            string apiKey = "AIzaSyCkRKueVwpwMPzUcWZpxqHW4myR1oNYfxM";
+            string apiUrl = $"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId={playlistId}&key={apiKey}";
+
+            using (WebClient client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                return client.DownloadString(apiUrl);
+            }
+        }
+
+        private string[] GetVideoTitles(string apiResponse)
+        {
+            JObject json = JObject.Parse(apiResponse);
+            JArray items = (JArray)json["items"];
+            int itemCount = items.Count;
+
+            string[] videoTitles = new string[itemCount];
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                videoTitles[i] = (string)items[i]["snippet"]["title"];
+            }
+
+            return videoTitles;
+        }
+
+        private void btnExtrair_Click(object sender, EventArgs e)
+        {
+            string playlistUrl = txtPlaylistUrl.Text;
+
+            if (IsValidPlaylistUrl(playlistUrl))
+            {
+                string playlistId = GetPlaylistId(playlistUrl);
+
+                if (!string.IsNullOrEmpty(playlistId))
+                {
+                    string apiResponse = GetApiResponse(playlistId);
+                    string[] videoTitles = GetVideoTitles(apiResponse);
+
+                    if (videoTitles.Length > 0)
+                    {
+                        SaveTitlesToFile(videoTitles);
+                        MessageBox.Show("Os Títulos dos videos foram salvos com sucesso");
+                    }
+                    else MessageBox.Show("Não foi possível encontrar os títulos de vídeos na playlist fornecida.");
+                }
+                else MessageBox.Show("Não foi possível obter o ID da playlist apartir da URL fornecida.");
+            }
+            else MessageBox.Show("A URL fornecida não é uma URL válida de uma playlist do Youtube");
+        }
+
+        private void SaveTitlesToFile(string[] videoTitles)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Arquivo de texto (*.txt)|*.txt;";
+                saveFileDialog.Title = "Salvar títulos dos vídeos";
+                saveFileDialog.FileName = "titles.txt";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllLines(saveFileDialog.FileName, videoTitles, Encoding.UTF8);
+                }
+            }
+        }
+    }
+}
