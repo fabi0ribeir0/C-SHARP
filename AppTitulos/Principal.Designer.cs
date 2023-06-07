587319d6ebb8f570eb2d3ef429339adcf6e51464
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-06-07 17:38:50 -0300

    Funcionando

---
+++ AppTitulos/Principal.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace AppTitulos
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExtrair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPlaylistUrl = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExtrair
+            // 
+            this.btnExtrair.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExtrair.Location = new System.Drawing.Point(12, 71);
+            this.btnExtrair.Name = "btnExtrair";
+            this.btnExtrair.Size = new System.Drawing.Size(75, 29);
+            this.btnExtrair.TabIndex = 0;
+            this.btnExtrair.Text = "Extrair";
+            this.btnExtrair.UseVisualStyleBackColor = true;
+            this.btnExtrair.Click += new System.EventHandler(this.btnExtrair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL da playlist";
+            // 
+            // txtPlaylistUrl
+            // 
+            this.txtPlaylistUrl.Location = new System.Drawing.Point(12, 45);
+            this.txtPlaylistUrl.Name = "txtPlaylistUrl";
+            this.txtPlaylistUrl.Size = new System.Drawing.Size(494, 20);
+            this.txtPlaylistUrl.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 124);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(504, 251);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 474);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtPlaylistUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExtrair);
+            this.Name = "Principal";
+            this.Text = "Youtube DL > Titulos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExtrair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPlaylistUrl;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ AppTitulos/Principal.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace AppTitulos
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private string RunYoutubeDLCommand(string playlistUrl)
+        {
+            string command = $"c:\\Youtubedl\\youtube-dl --ignore-errors --skip-download --get-title --get-id --flat-playlist {playlistUrl}";
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                Arguments = $"/c {command}"
+            };
+
+            using (var process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(output))
+                {
+                    return output;
+                }
+                else
+                {
+                    MessageBox.Show($"Erro ao executar o comando youtube-dl: {error}");
+                    return null;
+                }
+            }
+        }
+
+        private string[] ExtractVideoTitles(string output)
+        {
+            string[] lines = output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            return lines.Select(line => line.TrimStart()).ToArray();
+        }
+
+        private void SaveTitlesToFile(string[] videoTitles)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Arquivo de texto (*.txt)|*.txt;";
+                saveFileDialog.Title = "Salvar títulos dos vídeos";
+                saveFileDialog.FileName = "titles.txt";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    System.IO.File.WriteAllLines(saveFileDialog.FileName, videoTitles);
+                }
+            }
+        }
+
+        private void btnExtrair_Click(object sender, EventArgs e)
+        {
+            string playlistUrl = txtPlaylistUrl.Text;
+            string output = RunYoutubeDLCommand((playlistUrl));
+
+            if (!string.IsNullOrEmpty(output))
+            {
+                string[] videoTitles = ExtractVideoTitles(output);
+
+                if (videoTitles.Length > 0)
+                {
+                    listBox1.Items.Clear();
+                    listBox1.Items.AddRange(videoTitles);
+                    SaveTitlesToFile(videoTitles);
+                    MessageBox.Show("Os títulos dos videos foram salvos!");
+                }
+                else MessageBox.Show("Não foi possível encontrar títulos de vídeos na playlist fornecida.");
+            }
+            else MessageBox.Show("Não foi possível obter os detalhes da playlist usando youtube-dl.");
+        }
+    }
+}
